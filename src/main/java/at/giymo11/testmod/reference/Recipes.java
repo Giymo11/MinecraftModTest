8f6cd3e06979080108eb7fb3ca9a7f56172870be
Benjamin Potzmann <benjamin.potzmann@gmail.com>
2014-08-19 02:34:53 +0200

    Adds new OreDictionary Recipies

--- src/main/java/at/giymo11/testmod/reference/Recipes.java
+++ src/main/java/at/giymo11/testmod/reference/Recipes.java
@@ -1,14 +1,20 @@
 package at.giymo11.testmod.reference;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 /**
  * Created by Giymo11 on 19.08.2014.
  */
 public class Recipes {
     public static void registerRecipes() {
-        GameRegistry.addShapedRecipe(new ItemStack(Items.mapleLeaf), " s ", "sss", " s ", 's', new ItemStack(net.minecraft.init.Items.stick));
-        GameRegistry.addShapelessRecipe(new ItemStack(Blocks.austrianFlag), new ItemStack(Items.mapleLeaf), new ItemStack(net.minecraft.init.Blocks.wool));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.mapleLeaf), " s ", "sss", " s ", 's', "stickWood"));
+        //add all wools
+        for(int meta = 0; meta < 16; ++meta) {
+            GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Blocks.austrianFlag), new ItemStack(Items.mapleLeaf), new ItemStack(Item.getItemFromBlock(net.minecraft.init.Blocks.wool), 0, meta)));
+        }
     }
 }
